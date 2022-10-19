5a384938b6b6df984cfc8a48e708e8d9da4a14ba
Achref <medachrefg@gmail.com>
2022-10-19 04:37:13 +0100

    handling post req

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const exp = require('constants');
 const express = require('express');
 const fs = require('fs');
 
 app = express();
-
+app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -12,6 +13,26 @@ app.get('/api/v1/tours', (req, res, next) => {
     status: 'success',
     result: tours.length,
     tours,
+  });
+});
+
+app.post('/api/v1/tours', (req, res, next) => {
+  const newId = parseInt(tours[tours.length - 1].id) + 1;
+  console.log(tours.length - 1);
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => console.log(err)
+  );
+
+  res.status(201).send({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
   });
 });
 
